4291ff5a973c4a37d01a6696f585a502bff5147d
bhaskerath <bhaskerathikayala@gmail.com>
2021-03-26 14:18:44 +0530

    adding project fiels

---
+++ src/test/java/test/Login.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.apache.log4j.Logger;
+
+public class Login {
+
+	static Logger log = Logger.getLogger(Login.class);
+	String b = getClass().toString();
+	public static void main(String[] args) {
+		System.out.println(new Login().b);
+		System.out.println("Hello WOrld");
+		log.info("Bhaskar");
+		log.warn("Started");
+		log.fatal("fatal");
+		log.error("err");
+		log.debug("debug");
+		log.info(log.getClass());
+	}
+}
